e9208fe09e6be0fe89bc3ef0b15e53702794a4a8
yzp531 <yzp531@163.com>
2018-01-10 13:34:08 +0800

    3

--- MyApplication/app/src/main/cpp/native-lib.cpp
+++ MyApplication/app/src/main/cpp/native-lib.cpp
@@ -9,3 +9,9 @@ Java_com_example_jerryyin_myapplication_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+const jobject
+callJavaMethodBase(JNIEnv *env, jobject obj, char *clzName, char *methodName, char *paramsType,
+                   char *paramVal) {
+   jclass  o=env->FindClass(clzName);
+}
